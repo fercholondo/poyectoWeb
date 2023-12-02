c7d62eb874490149cb3eefdfc08b531ef6ad7775
fercholondo <luis.londono4@udea.edu.co>
2023-12-02 04:05:14 -0500

    se realiza la creacion de añadir y borra empleado

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const fecha_ingreso = document.querySelector(".fecha_ingreso");
 const cargo = document.querySelector(".cargo");
 const vinculado = document.querySelector(".es_vinculado");
 const salario = document.querySelector(".salario");
+const botonCrearEmpleado = document.querySelector(".crear button");
 
 botonBuscarEmpleado.addEventListener("click", async () => {
     try {
@@ -28,7 +29,7 @@ botonBuscarEmpleado.addEventListener("click", async () => {
    vinculado.innerHTML = response.data["es_vinculado"];
    cargo.innerHTML = response.data["cargo"];
    salario.innerHTML = response.data["salario"];
-
+console.log(typeof(nombres.value))
 
   //   const temperatura = respuesta.data["main"]["temp"];    
    // temperaturas.innerHTML = `${Math.round(temperatura)}°C`;
@@ -41,4 +42,57 @@ botonBuscarEmpleado.addEventListener("click", async () => {
      console.error("Error al buscar el Id", error);   
      window.alert("Error al buscar el Id") 
   }
+});
+/*botonCrearEmpleado.addEventListener("click", async () => {
+  try {
+  
+  
+  // Realiza la solicitud a la API principal
+ const response = await axios.post(
+    `http://localhost:8080/empleados`
+        );
+  ident=Number(document.querySelector("#identificacion" ).value)
+  nomb=document.querySelector("#nombres" ).value
+  apelli=document.querySelector("#apellidos" ).value
+  fecha_in=document.querySelector("#fecha_ingreso" ).value
+  car=document.querySelector("#cargo" ).value
+  es_vin=Boolean(document.querySelector("#es_vinculado" ).value)
+  salar=String(document.querySelector("#salario" ).value)
+
+  console.log(typeof(ident),typeof(nomb),typeof(apelli),typeof(fecha_in),typeof(car),typeof(es_vin),typeof(salar)
+  )
+
+  empleado={"identificacion":ident,
+  "nombres":nomb,
+  "apellidos":apelli,
+  "fecha_ingreso":fecha_in,
+  "cargo":car,
+  "es_vinculado":es_vin,
+  "salario":salar}
+  console.log (empleado)
+
+ 
+
+} catch (error) {
+   // Manejo de errores
+   console.error("Error al crear empleado", error);   
+   window.alert("Error  al crear empleado") 
+}
+});*/
+botonCrearEmpleado.addEventListener("click", async () => {
+  try {
+    console.log(Id.value)
+  
+  
+  // Realiza la solicitud a la API principal
+ const response = await axios.delete(
+    `http://localhost:8080/empleados/${Id.value}`
+        );
+        console.log("si paso")
+  
+  } catch (error) {
+   // Manejo de errores
+   console.error("Error al eliminar empleado", error);   
+   window.alert("Error  al eliminar empleado") 
+}
 });
